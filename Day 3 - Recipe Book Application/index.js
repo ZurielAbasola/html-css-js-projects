9e271256eb3fd11001ca08d943803545cfe30007
ZurielAbasola <zuriel123.abasola@gmail.com>
2023-06-22 23:56:00 -0600

    Image not showing up

--- Day 3 - Recipe Book Application/index.js
+++ Day 3 - Recipe Book Application/index.js
@@ -1,5 +1,20 @@
 const API_KEY = "275d58779ccf4e22af03e792e8819fff";
-const recipeListElement = document.getElementById("recipe-list");
+const recipeListE1 = document.getElementById("recipe-list");
+
+function displayRecipes(recipes) {
+    recipeListElement.innerHTML = "";
+        recipes.forEach(recipe => {
+            const recipeItemE1 = document.createElement("li");
+            recipeItemE1.classList.add("recipe-item");
+            const recipeImageE1 = document.createElement("img");
+            recipeImageE1.src = recipe.image;
+            recipeImageE1.alt = "recipe image";
+
+            recipeItemE1.appendChild(recipeImageE1);
+            
+            recipeListE1.appendChild(recipeItemE1);
+        })
+}
 
 async function getRecipes() {
     const response = await fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`);
@@ -11,13 +26,7 @@ async function getRecipes() {
 async function init() {
     const recipes = await getRecipes();
 
-    recipes => {
-        recipeListElement.innerHTML = "";
-        recipes.forEach(recipe => {
-            const recipeElement1 = document.createElement("li");
-            recipeElement1.classList
-        })
-    }
+    displayRecipes(recipes);
 }
 
 init();
